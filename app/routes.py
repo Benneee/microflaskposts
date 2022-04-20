65b2a0f62e29b3d04b608695ed0d7224a4cf6b2f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-04-20 17:49:18 +0100

    Feat: show posts on homepage

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, redirect, url_for
+from flask import flash, redirect, render_template, redirect, url_for, request
 from app import app, db
 from app.models import Post
 from app.forms import FlaskPostForm
@@ -7,10 +7,14 @@ from app.forms import FlaskPostForm
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = FlaskPostForm()
+    posts = []
     if form.validate_on_submit():
         post = Post(title=form.title.data, body=form.body.data)
         db.session.add(post)
         db.session.commit()
         flash('Post added successfully!', category="success")
         return redirect(url_for('index'))
-    return render_template('index.html', title="MicroFlaskPosts", form=form)
+    elif request.method == 'GET':
+        page = request.args.get('page', 1, type=int)
+        posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', title="MicroFlaskPosts", form=form, posts=posts.items)
