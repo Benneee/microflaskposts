a3c8c5f6e4fc1a5bc071067fce51ecfe28cb51fb
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-04-28 20:00:36 +0100

    WIP: adding authentication functionalities

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,35 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User
 
 
 class FlaskPostForm(FlaskForm):
     title = StringField(label='Title', validators=[DataRequired()])
     body = TextAreaField(label='Body', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField(label='Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
+    submit = SubmitField('Sign Up')
+
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise ValidationError('This email address is taken. Please, use another one')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('This username is taken. Please, use another one')
