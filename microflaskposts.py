0931b8f402aea52096a17d076249e0a2a3efe0f6
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-04-30 08:45:04 +0100

    Feat: added user model to shell context:

--- microflaskposts.py
+++ microflaskposts.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models import Post
+from app.models import Post, User
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         'db': db,
-        'Post': Post
+        'Post': Post,
+        'User': User
     }
